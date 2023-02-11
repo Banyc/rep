fbf52eea37539798874b17d200a74805782d3b6b
Banyc <36535895+Banyc@users.noreply.github.com>
2023-02-12 04:05:01 +0800

    refactor: encap condition list

---
+++ rep_derive/src/check_indie_fields.rs
@@ -0,0 +1,161 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{spanned::Spanned, Error, Field, Lit, Meta};
+
+pub fn check_indie_fields(
+    meta: &Meta,
+    field: &Field,
+    check_conditions: &mut Vec<TokenStream>,
+    check_errors: &mut Vec<TokenStream>,
+    errors: &mut Vec<TokenStream>,
+    fields_to_recurse_on: &mut Vec<syn::Ident>,
+) {
+    let Some(field_ident) = &field.ident else {
+        errors.push(Error::new(field.span(), "expected named fields").to_compile_error());
+        return;
+    };
+    let field_ident_str = field_ident.to_string();
+    let field_type = &field.ty;
+    match meta {
+        Meta::Path(p) => {
+            if p.is_ident("check") {
+                fields_to_recurse_on.push(field_ident.clone());
+            } else if p.is_ident("assert_default") {
+                check_conditions.push(quote! {
+                    {
+                        let default: #field_type = Default::default();
+                        self.#field_ident == default
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be default, not {}", #field_ident_str, self.#field_ident)
+                });
+            } else if p.is_ident("assert_true") {
+                check_conditions.push(quote! {
+                    {
+                        self.#field_ident
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be true", #field_ident_str)
+                });
+            } else if p.is_ident("assert_false") {
+                check_conditions.push(quote! {
+                    {
+                        !self.#field_ident
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be false", #field_ident_str)
+                });
+            } else {
+                errors.push(
+                    Error::new(p.span(), "unsupported representation invariant").to_compile_error(),
+                );
+            }
+        }
+        Meta::NameValue(v) => {
+            if v.path.is_ident("assert_eq") {
+                let val = v.lit.clone();
+                check_conditions.push(quote! {
+                    {
+                        self.#field_ident == #val
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be {}, not {}", #field_ident_str, #val, self.#field_ident)
+                });
+            } else if v.path.is_ident("assert_ne") {
+                let val = v.lit.clone();
+                check_conditions.push(quote! {
+                    {
+                        self.#field_ident != #val
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must not be {}", #field_ident_str, #val)
+                });
+            } else if v.path.is_ident("assert_gt") {
+                let val = v.lit.clone();
+                check_conditions.push(quote! {
+                    {
+                        self.#field_ident > #val
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be > {}, not {}", #field_ident_str, #val, self.#field_ident)
+                });
+            } else if v.path.is_ident("assert_lt") {
+                let val = v.lit.clone();
+                check_conditions.push(quote! {
+                    {
+                        self.#field_ident < #val
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be < {}, not {}", #field_ident_str, #val, self.#field_ident)
+                });
+            } else if v.path.is_ident("assert_ge") {
+                let val = v.lit.clone();
+                check_conditions.push(quote! {
+                    {
+                        self.#field_ident >= #val
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be >= {}, not {}", #field_ident_str, #val, self.#field_ident)
+                });
+            } else if v.path.is_ident("assert_le") {
+                let val = v.lit.clone();
+                check_conditions.push(quote! {
+                    {
+                        self.#field_ident <= #val
+                    }
+                });
+                check_errors.push(quote! {
+                    format!("self.{} must be <= {}, not {}", #field_ident_str, #val, self.#field_ident)
+                });
+            } else if v.path.is_ident("assert_with") {
+                let val = v.lit.clone();
+                if let Lit::Str(fn_name) = val.clone() {
+                    let fn_to_call = fn_name.parse::<syn::Path>();
+                    if let Ok(fn_to_call) = fn_to_call.clone() {
+                        check_conditions.push(quote! {
+                            {
+                                #fn_to_call ( self.#field_ident )
+                            }
+                        });
+                        check_errors.push(quote! {
+                            format!("{}(self.{}) must be true when self.{} == {}", #fn_name, #field_ident_str, #field_ident_str, self.#field_ident)
+                        });
+                    } else {
+                        errors.push(
+                            Error::new(
+                                val.span(),
+                                "assert_with can only be used with the name of a function to call",
+                            )
+                            .to_compile_error(),
+                        );
+                    }
+                } else {
+                    errors.push(
+                        Error::new(
+                            val.span(),
+                            "assert_with can only be used with the name of a function to call",
+                        )
+                        .to_compile_error(),
+                    );
+                }
+            } else {
+                errors.push(
+                    Error::new(v.span(), "unsupported representation invariant").to_compile_error(),
+                );
+            }
+        }
+        _ => {
+            errors.push(
+                Error::new(meta.span(), "unsupported representation invariant").to_compile_error(),
+            );
+        }
+    }
+}
--- rep_derive/src/lib.rs
+++ rep_derive/src/lib.rs
@@ -1,5 +1,7 @@
+mod check_indie_fields;
 mod utils;
 
+use check_indie_fields::*;
 use utils::*;
 
 extern crate proc_macro;
@@ -8,8 +10,7 @@ use quote::quote;
 use quote::ToTokens;
 use syn::spanned::Spanned;
 use syn::{
-    parse_macro_input, Data, DeriveInput, Error, Fields, ImplItemMethod, ItemImpl, Lit, Meta,
-    NestedMeta,
+    parse_macro_input, Data, DeriveInput, Error, ImplItemMethod, ItemImpl, Meta, NestedMeta,
 };
 
 /// A macro for deriving an implementation of `CheckRep`
@@ -32,192 +33,35 @@ pub fn derive_check_indie_fields(input: proc_macro::TokenStream) -> proc_macro::
     let name = input.ident;
     let data = input.data;
 
-    let mut checks = vec![];
+    let mut check_conditions = vec![];
     let mut check_errors = vec![];
     let mut errors = vec![];
     let mut fields_to_recurse_on = vec![];
 
     if let Data::Struct(data_struct) = data {
-        let fields = data_struct.fields;
-        let mut fields_contents = None;
-        if let Fields::Named(fields_named) = fields {
-            fields_contents = Some(fields_named.named);
-        // } else if let Fields::Unnamed(fields_unnamed) = fields {
-        //     fields_contents = Some(fields_unnamed.unnamed);
-        } else {
-            errors.push(Error::new(fields.span(), "expected named fields").to_compile_error());
-        }
-
-        for (_, field) in fields_contents.unwrap().iter().enumerate() {
+        for (_, field) in data_struct.fields.into_iter().enumerate() {
             for attr in field.attrs.clone() {
                 let maybe_meta = attr.parse_meta();
 
                 if let Ok(meta) = maybe_meta {
                     if let Meta::List(meta_list) = meta {
                         if meta_list.path.is_ident("rep") {
-                            if meta_list.nested.len() == 1 {
-                                let nested = meta_list.nested.first().unwrap();
-
-                                // #[rep] comes in 2 varieties
-                                // 1. literals like #[rep(eq ="my_func")]
-                                // 2. paths like #[rep(always_true)]
-                                if let NestedMeta::Meta(nested_meta) = nested {
-                                    let field_name = field.ident.clone().unwrap(); //_or(Ident::new(&i.to_string(), Span::call_site()));
-                                    let field_name_name = field.ident.clone().unwrap().to_string();
-                                    let field_type = field.ty.clone();
-                                    match nested_meta {
-                                        Meta::Path(p) => {
-                                            if p.is_ident("check") {
-                                                fields_to_recurse_on.push(field_name);
-                                            } else if p.is_ident("assert_default") {
-                                                checks.push(quote! {
-                                                    {
-                                                        let default: #field_type = Default::default();
-                                                        self.#field_name == default
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be default, not {}", #field_name_name, self.#field_name)
-                                                });
-                                            } else if p.is_ident("assert_true") {
-                                                checks.push(quote! {
-                                                    {
-                                                        self.#field_name
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be true", #field_name_name)
-                                                });
-                                            } else if p.is_ident("assert_false") {
-                                                checks.push(quote! {
-                                                    {
-                                                        !self.#field_name
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be false", #field_name_name)
-                                                });
-                                            } else {
-                                                errors.push(
-                                                    Error::new(
-                                                        p.span(),
-                                                        "unsupported representation invariant",
-                                                    )
-                                                    .to_compile_error(),
-                                                );
-                                            }
-                                        }
-                                        Meta::NameValue(v) => {
-                                            if v.path.is_ident("assert_eq") {
-                                                let val = v.lit.clone();
-                                                checks.push(quote! {
-                                                    {
-                                                        self.#field_name == #val
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be {}, not {}", #field_name_name, #val, self.#field_name)
-                                                });
-                                            } else if v.path.is_ident("assert_ne") {
-                                                let val = v.lit.clone();
-                                                checks.push(quote! {
-                                                    {
-                                                        self.#field_name != #val
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must not be {}", #field_name_name, #val)
-                                                });
-                                            } else if v.path.is_ident("assert_gt") {
-                                                let val = v.lit.clone();
-                                                checks.push(quote! {
-                                                    {
-                                                        self.#field_name > #val
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be > {}, not {}", #field_name_name, #val, self.#field_name)
-                                                });
-                                            } else if v.path.is_ident("assert_lt") {
-                                                let val = v.lit.clone();
-                                                checks.push(quote! {
-                                                    {
-                                                        self.#field_name < #val
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be < {}, not {}", #field_name_name, #val, self.#field_name)
-                                                });
-                                            } else if v.path.is_ident("assert_ge") {
-                                                let val = v.lit.clone();
-                                                checks.push(quote! {
-                                                    {
-                                                        self.#field_name >= #val
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be >= {}, not {}", #field_name_name, #val, self.#field_name)
-                                                });
-                                            } else if v.path.is_ident("assert_le") {
-                                                let val = v.lit.clone();
-                                                checks.push(quote! {
-                                                    {
-                                                        self.#field_name <= #val
-                                                    }
-                                                });
-                                                check_errors.push(quote! {
-                                                    format!("self.{} must be <= {}, not {}", #field_name_name, #val, self.#field_name)
-                                                });
-                                            } else if v.path.is_ident("assert_with") {
-                                                let val = v.lit.clone();
-                                                if let Lit::Str(fn_name) = val.clone() {
-                                                    let fn_to_call = fn_name.parse::<syn::Path>();
-                                                    if let Ok(fn_to_call) = fn_to_call.clone() {
-                                                        checks.push(quote! {
-                                                        {
-                                                            #fn_to_call ( self.#field_name )
-                                                        }
-                                                        });
-                                                        check_errors.push(quote! {
-                                                            format!("{}(self.{}) must be true when self.{} == {}", #fn_name, #field_name_name, #field_name_name, self.#field_name)
-                                                        });
-                                                    } else {
-                                                        errors.push(Error::new(val.span(), "assert_with can only be used with the name of a function to call").to_compile_error());
-                                                    }
-                                                } else {
-                                                    errors.push(Error::new(val.span(), "assert_with can only be used with the name of a function to call").to_compile_error());
-                                                }
-                                            } else {
-                                                errors.push(
-                                                    Error::new(
-                                                        v.span(),
-                                                        "unsupported representation invariant",
-                                                    )
-                                                    .to_compile_error(),
-                                                );
-                                            }
-                                        }
-                                        _ => {
-                                            errors.push(
-                                                Error::new(
-                                                    nested_meta.span(),
-                                                    "unsupported representation invariant",
-                                                )
-                                                .to_compile_error(),
-                                            );
-                                        }
-                                    }
+                            for meta in meta_list.nested {
+                                if let NestedMeta::Meta(meta) = meta {
+                                    check_indie_fields(
+                                        &meta,
+                                        &field,
+                                        &mut check_conditions,
+                                        &mut check_errors,
+                                        &mut errors,
+                                        &mut fields_to_recurse_on,
+                                    );
                                 } else {
                                     errors.push(
-                                        Error::new(nested.span(), "invalid usage of #[rep]")
+                                        Error::new(meta.span(), "invalid usage of #[rep]")
                                             .to_compile_error(),
                                     );
                                 }
-                            } else {
-                                errors.push(
-                                    Error::new(meta_list.span(), "expected just 1 item")
-                                        .to_compile_error(),
-                                );
                             }
                         }
                     }
@@ -228,19 +72,22 @@ pub fn derive_check_indie_fields(input: proc_macro::TokenStream) -> proc_macro::
         errors.push(Error::new(name.span(), "expected name of structure").to_compile_error());
     }
 
-    let expanded = if errors.len() > 0 {
-        quote! {
-            #(#errors)*
-        }
-    } else {
-        quote! {
-            impl rep::CheckIndieFields for #name {
-                fn check_indie_fields(&self, e: &mut RepErrors) {
-                    #( if ! #checks { e.add( #check_errors ); } )*
-                    #(
-                        self. #fields_to_recurse_on .check_indie_fields(e);
-                    )*
+    let expanded = match errors.is_empty() {
+        true => {
+            quote! {
+                impl rep::CheckIndieFields for #name {
+                    fn check_indie_fields(&self, e: &mut RepErrors) {
+                        #( if ! #check_conditions { e.add( #check_errors ); } )*
+                        #(
+                            self. #fields_to_recurse_on .check_indie_fields(e);
+                        )*
+                    }
                 }
+            }
+        }
+        false => {
+            quote! {
+                #(#errors)*
             }
         }
     };
